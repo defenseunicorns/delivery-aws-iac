a579b38e1f2af6a7200f7b54bb597d7222f3d4e6
Gabe Scarberry <gabriel.scarberry.1@us.af.mil>
2023-02-17 15:30:21 -0600

    move data to data.tf in eks module

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,27 +1,3 @@
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks_blueprints.eks_cluster_id
-}
-
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-data "aws_ami" "amazonlinux2eks" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amazon-eks-node-${var.eks_k8s_version}-*"]
-  }
-
-  owners = ["amazon"]
-}
-
-data "aws_partition" "current" {}
-
 #---------------------------------------------------------------
 # EKS Blueprints
 #---------------------------------------------------------------
@@ -135,19 +111,6 @@ EOF
 #---------------------------------------------------------------
 # Custom IAM role for Self Managed Node Group
 #---------------------------------------------------------------
-data "aws_iam_policy_document" "self_managed_ng_assume_role_policy" {
-  statement {
-    sid = "EKSWorkerAssumeRole"
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_role" "self_managed_ng" {
 
@@ -186,19 +149,6 @@ resource "aws_iam_instance_profile" "self_managed_ng" {
 #---------------------------------------------------------------
 # Custom IAM role for Managed Node Group
 #---------------------------------------------------------------
-data "aws_iam_policy_document" "managed_ng_assume_role_policy" {
-  statement {
-    sid = "EKSWorkerAssumeRole"
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_role" "managed_ng" {
 
