32532cdea9b1a66e71d6f8286bc2a5b9dd74a0b9
Zack Annexstein <zack.annexstein@defenseunicorns.com>
2023-02-27 11:00:38 -0800

    refactor

--- examples/tf-state-backend/main.tf
+++ examples/tf-state-backend/main.tf
@@ -5,14 +5,17 @@ provider "aws" {
 data "aws_partition" "current" {}
 
 module "tfstate_backend" {
-  source = "git::https://github.com/defenseunicorns/iac.git//modules/tfstate-backend?ref=v0.0.0-alpha.2"
+  source = "git::https://github.com/defenseunicorns/terraform-aws-tfstate-backend"
 
   region              = var.region
   bucket_prefix       = "my-tfstate-backend"
   dynamodb_table_name = "my-tfstate-backend-lock"
 
   # list of admin's AWS account arn to allow control of KMS keys
-  cluster_key_admin_arns = ["arn:${data.aws_partition.current.partition}:iam::${var.account}:user/${var.aws_admin_1_username}", "arn:${data.aws_partition.current.partition}:iam::${var.account}:user/${var.aws_admin_2_username}"]
+  cluster_key_admin_arns = [
+    "arn:${data.aws_partition.current.partition}:iam::${var.account}:user/${var.aws_admin_1_username}",
+    "arn:${data.aws_partition.current.partition}:iam::${var.account}:user/${var.aws_admin_2_username}"
+  ]
 }
 
 output "tfstate_bucket_id" {
