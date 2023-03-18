a65e390009f905bc5698d9ef58bf688de5197a33
Zack Annexstein <zannexstein@gmail.com>
2023-03-17 21:47:01 -0700

    make list

--- modules/eks/locals.tf
+++ modules/eks/locals.tf
@@ -10,7 +10,7 @@ locals {
   }
   admin_arns = distinct(concat(
     [for admin_user in var.aws_admin_usernames : "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"],
-    data.aws_caller_identity.current.arn
+    [data.aws_caller_identity.current.arn]
   ))
   aws_auth_users = distinct(concat([for admin_user in var.aws_admin_usernames : {
     userarn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
