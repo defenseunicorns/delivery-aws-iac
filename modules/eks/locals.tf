9a204e46a17967e93cf89864c36f4c34a5c346a5
Zack Annexstein <zannexstein@gmail.com>
2023-03-17 21:40:02 -0700

    1 more

--- modules/eks/locals.tf
+++ modules/eks/locals.tf
@@ -8,13 +8,21 @@ locals {
     Blueprint  = var.name
     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
   }
-  admin_arns = [for admin_user in var.aws_admin_usernames : "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"]
-  aws_auth_users = [for admin_user in var.aws_admin_usernames : {
+  admin_arns = distinct(concat(
+    [for admin_user in var.aws_admin_usernames : "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"],
+    data.aws_caller_identity.current.arn
+  ))
+  aws_auth_users = distinct(concat([for admin_user in var.aws_admin_usernames : {
     userarn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
     username = admin_user
     groups   = ["system:masters"]
-    }
-  ]
+    }],
+    [{
+      userarn  = data.aws_caller_identity.current.arn
+      username = split("/", data.aws_caller_identity.current.arn)[1]
+      groups   = ["system:masters"]
+    }]
+  ))
 
   cluster_addons = {
     vpc-cni = lookup(var.amazon_eks_vpc_cni, "enabled", false) ? {
