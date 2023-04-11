43cc800d5a7066a3f28493bcd3d5c362eb9a1272
Zack Annexstein <zannexstein@gmail.com>
2023-04-11 10:32:00 -0700

    sure

--- modules/bastion/s3-buckets.tf
+++ modules/bastion/s3-buckets.tf
@@ -185,10 +185,6 @@ resource "aws_s3_bucket_public_access_block" "session_logs_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
-  # The ACL needs to be added before this public access block can be applied. If the public access block gets applied first, then the ACL gets is not able to be added, leading to a race condition.
-  # https://stackoverflow.com/questions/71080354/getting-the-bucket-does-not-allow-acls-error
-  depends_on = [aws_s3_bucket_acl.session_logs_bucket]
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "session_logs_bucket" {
