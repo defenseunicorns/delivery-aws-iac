7cfbf834f6ddf72ed9d9595bd664433663427ed2
jase koonce <jase@defenseunicorns.com>
2023-06-14 10:32:54 -0500

    adding test for variable validation
    Signed-off-by: jase koonce <jase@defenseunicorns.com>

---
+++ test/e2e/examples_variable_test.go
@@ -0,0 +1,34 @@
+package e2e_test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	teststructure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestExamplesVariable(t *testing.T) {
+	t.Parallel()
+	tempFolder := teststructure.CopyTerraformFolderToTemp(t, "../..", "examples/complete")
+	terraformOptionsPlan := &terraform.Options{
+		TerraformDir: tempFolder,
+		Upgrade:      false,
+		VarFiles: []string{
+			"fixtures.common.tfvars",
+			"fixtures.insecure.tfvars",
+		},
+	// Set any overrides for variables you would like to validate
+		Vars: map[string]interface{}{
+			"keycloak_enabled": false,
+		},
+		SetVarsAfterVarFiles: true,
+	}
+	teststructure.RunTestStage(t, "TERRAFORM_INIT", func() {
+		terraform.Init(t, terraformOptionsPlan)
+	})
+	// Run `terraform plan` with the specified variable
+	teststructure.RunTestStage(t, "TERRAFORM_PLAN", func() {
+		terraform.Plan(t, terraformOptionsPlan)
+	})
+}
+
+	
