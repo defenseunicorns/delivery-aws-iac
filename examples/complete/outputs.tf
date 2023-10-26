9f0601f8f1dd5c3df9ef9c27a0a6e7884ae5df1b
bunchmj <matt.bunch@defenseunicorns.com>
2023-10-25 22:42:15 -0500

    reference fix?

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -38,5 +38,5 @@ output "efs_storageclass_name" {
 
 output "lambda_password_function_arn" {
   description = "Arn for lambda password function"
-  value       = try(module.password_lambda[0].lambda_password_function_arn)
+  value       = try(module.password_lambda[0].lambda_function_arn)
 }
