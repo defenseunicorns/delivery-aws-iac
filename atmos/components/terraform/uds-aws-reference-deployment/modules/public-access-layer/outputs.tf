6dad0682303e6f3f0d6c23f45ff004267b10d7f7
Gabe Scarberry <ntwkninja@gmail.com>
2025-01-09 15:54:56 -0600

    remove components

--- atmos/components/terraform/uds-aws-reference-deployment/modules/public-access-layer/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "public_vpc_properties" {
-  value = module.public_vpc.vpc_properties
-}
-
-
-output "public_lb_properties" {
-  value = local.public_lb_properties
-}
--- atmos/components/terraform/uds-aws-reference-deployment/modules/public-access-layer/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "public_access_layer_requirements" {
-  type = object({
-    azs                     = list(string)
-    deployment_requirements = any
-    private_vpc_properties  = any
-    vpc_requirements        = any
-  })
-}
